37bc971a0108b7ede81948203937fc87c1bc270d
HAL323 <konabou323@gmail.com>
2025-08-02 13:19:27 +0900

    2025/8/2　岡野暖音　gameシーンにセッティングを持ってきました

--- team_B2_janken/Assets/Scripts/Game/Player1Controller.cs
+++ team_B2_janken/Assets/Scripts/Game/Player1Controller.cs
@@ -13,14 +13,17 @@ public class Player1Controller : MonoBehaviour
         if (selected[0] == 1)
         {
             Debug.Log("設定された手：グー");
+            gameObject.tag = "Rock";
         }
         else if (selected[1] == 1)
         {
             Debug.Log("設定された手：チョキ");
+            gameObject.tag = "Scissors";
         }
         else if (selected[2] == 1)
         {
             Debug.Log("設定された手：パー");
+            gameObject.tag = "Paper";
         }
         else
         {
