8cefcb78efde8b1ba36a55760b7dc4893658e53b
DAIPANPC\mozae <mozaemon088@gmail.com>
2025-08-02 14:20:48 +0900

    2025/08/02 音田悠介　Playerスクリプトなどの追加（修正中）Iuput Systemの追加

--- team_B2_janken/Assets/Scripts/Game/Player2Controller.cs
+++ team_B2_janken/Assets/Scripts/Game/Player2Controller.cs
@@ -1,18 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+// クラス名だけ Player2Controller に変更
 public class Player2Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float walkSpeed = 5f;
+    public float dashSpeed = 10f;
+    public float jumpForce = 5f;
+    public float rotationSpeed = 200f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+    private Transform cameraTransform;
+
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private bool isDash;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        cameraTransform = GetComponentInChildren<Camera>().transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float currentSpeed = isDash ? dashSpeed : walkSpeed;
         
+        Vector3 cameraForward = Vector3.Scale(cameraTransform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 finalMoveDirection = cameraForward * moveInput.y + cameraTransform.right * moveInput.x;
+
+        if (finalMoveDirection != Vector3.zero)
+        {
+            transform.forward = Vector3.Slerp(transform.forward, finalMoveDirection, Time.fixedDeltaTime * 10f);
+        }
+
+        rb.velocity = new Vector3(finalMoveDirection.x * currentSpeed, rb.velocity.y, finalMoveDirection.z * currentSpeed);
+        
+        transform.Rotate(Vector3.up * lookInput.x * rotationSpeed * Time.fixedDeltaTime);
+    }
+    
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+    
+    public void OnJump(InputValue value)
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
+        if (value.isPressed && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+    
+    public void OnDash(InputValue value)
+    {
+        isDash = value.isPressed;
     }
 }
