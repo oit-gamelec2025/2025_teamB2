19ae17f853ed028831be2827ab5209ce5f3d9ac0
HAL323 <konabou323@gmail.com>
2025-08-03 12:47:44 +0900

    2025/8/3　岡野暖音　ブランチテスト

--- team_B2_janken/Assets/Scripts/Game/Player1Controller.cs
+++ team_B2_janken/Assets/Scripts/Game/Player1Controller.cs
@@ -4,7 +4,6 @@ using UnityEngine.InputSystem; // 新Input System
 public class Player1Script : MonoBehaviour
 {
     Rigidbody player1RigidBody;
-    float speed = 3.0f;
     Vector2 moveInput; // 左スティックの入力を保存する
     public GameObject Camera;
     private Vector3 _velocity;
@@ -26,5 +25,40 @@ public class Player1Script : MonoBehaviour
     {
         // オブジェクト移動
         transform.position += _velocity * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (gameObject.tag == "Rock" && other.gameObject.tag == "Scissors")
+        {
+            Debug.Log("Win");
+        }
+        if (gameObject.tag == "Paper" && other.gameObject.tag == "Rock")
+        {
+            Debug.Log("Win");
+        }
+        if (gameObject.tag == "Scissors" && other.gameObject.tag == "Paper")
+        {
+            Debug.Log("Win");
+        }
+
+        if (gameObject.tag == "Rock" && other.gameObject.tag == "Paper")
+        {
+            Debug.Log("Lose");
+        }
+        if (gameObject.tag == "Paper" && other.gameObject.tag == "Scissors")
+        {
+            Debug.Log("Lose");
+        }
+        if (gameObject.tag == "Scissors" && other.gameObject.tag == "Rock")
+        {
+            Debug.Log("Lose");
+        }
+
+        if(gameObject.tag == other.gameObject.tag)
+        {
+            Debug.Log("Draw");
+        }
+
     }
 }
--- team_B2_janken/Assets/Scripts/Game/Player2Controller.cs
+++ team_B2_janken/Assets/Scripts/Game/Player2Controller.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using UnityEngine.InputSystem; // 新Input System
 
 public class Player2Script : MonoBehaviour
@@ -28,3 +28,4 @@ public class Player2Script : MonoBehaviour
         transform.position += _velocity * Time.deltaTime;
     }
 }
+*/
